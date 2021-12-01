3309d589de51e580bd295137e40d73349b4d14ad
root <root@zeppelin.hakier.it>
2021-12-01 09:53:55 +0100

    bug: fix read of apps list
    Only first line from apps list was used.

--- usr/local/sbin/install-apps.sh
+++ usr/local/sbin/install-apps.sh
@@ -2,6 +2,9 @@
 
 appsFile=/usr/local/etc/apps
 
+echo 'deb http://deb.debian.org/debian/ bullseye main non-free contrib' > /etc/apt/sources.list.d/debian--non-free.list
+apt update
+
 editor "${appsFile}"
-apt install < "${appsFile}"
+apt install $(cat "${appsFile}")
 
